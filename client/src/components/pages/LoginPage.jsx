185b2e467201715320cce9b9628b75434e83da40
Yash1Hingu <yash23hingu@gmail.com>
2024-01-21 00:00:40 +0530

    Added Register Page with style

--- client/src/components/pages/LoginPage.jsx
+++ client/src/components/pages/LoginPage.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function LoginPage(){
     return (<div className="grow flex flex-col items-center justify-center mb-32">
         <h1 className="text-4xl text-center mb-4">Login</h1>
@@ -5,6 +7,9 @@ export default function LoginPage(){
             <input type="email" placeholder="your@gmail.com"/>
             <input type="password" placeholder="password"/>
             <button className="primary">Login</button>
+            <div className="text-gray-400 mt-2 text-center">Don't have an account yet?
+                <Link to={'/register'} className="text-black underline"> Register</Link>
+            </div>
         </form>
     </div>)
 }
--- client/src/components/pages/RegisterPage.jsx
+++ client/src/components/pages/RegisterPage.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function RegisterPage(){
-    return <>
-        <h1>Register Page</h1>
-    </>
+    return (<div className="grow flex flex-col items-center justify-center mb-32">
+    <h1 className="text-4xl text-center mb-4">Register</h1>
+    <form className="max-w-md mx-auto">
+        <input type="text" placeholder="john doe"/>
+        <input type="email" placeholder="your@gmail.com"/>
+        <input type="password" placeholder="password"/>
+        <button className="primary">Register</button>
+        <div className="text-gray-400 mt-2 text-center">you'r already meamber?
+            <Link to={'/login'} className="text-black underline"> Login</Link>
+        </div>
+    </form>
+</div>)
 }
