58b40fa92c4578e4ba2e43d66d0a0c26b1314e2d
Yash1Hingu <yash23hingu@gmail.com>
2024-02-14 01:03:14 +0530

    Fixed close btn

--- client/src/components/pages/PlacePage.jsx
+++ client/src/components/pages/PlacePage.jsx
@@ -29,7 +29,7 @@ export default function PlacePage() {
                         />
                     ))}
                 </div>
-                <button onClick={(ev) => showAllPhotos(ev, false)} className="flex absolute top-4 right-4 py-2 px-4 rounded-3xl gap-2 hover:text-red-600">
+                <button onClick={(ev) => showAllPhotos(ev, false)} className="flex fixed top-4 right-4 py-2 px-4 rounded-3xl gap-2 hover:text-red-600">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                         <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
                     </svg>
