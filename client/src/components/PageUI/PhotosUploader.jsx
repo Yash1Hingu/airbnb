56c117b17226c532667567fbe73d7c205fbc6347
Yash1Hingu <yash23hingu@gmail.com>
2024-02-13 20:51:09 +0530

    Added Remove Image feture

--- client/src/components/PageUI/PhotosUploader.jsx
+++ client/src/components/PageUI/PhotosUploader.jsx
@@ -37,6 +37,9 @@ export default function PhotosUploader({ addedPhotos, setAddedPhotos }) {
         })
     }
 
+    function removePhoto(filename){
+        setAddedPhotos([...addedPhotos.filter(photo => photo !== filename)]);
+    }
 
     return (
         <>
@@ -52,7 +55,7 @@ export default function PhotosUploader({ addedPhotos, setAddedPhotos }) {
                     />
                     {!isLoading &&
                         <button onClick={addPhotoByLink} className="bg-primary text-white p-2 rounded-lg"
-                            disabled={photoLink==='' ? true : false}
+                            disabled={photoLink === '' ? true : false}
                         >
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15M9 12l3 3m0 0 3-3m-3 3V2.25" />
@@ -66,12 +69,18 @@ export default function PhotosUploader({ addedPhotos, setAddedPhotos }) {
                 <div className="flex flex-wrap mt-2 w-full items-center gap-2">
                     {addedPhotos.length > 0 &&
                         addedPhotos.map(link => (
-                            <div className='h-32 flex' key={link}>
+                            <div className='h-32 flex relative' key={link}>
                                 <img
                                     src={'http://localhost:4000/upload/' + link}
                                     alt=""
                                     className='w-full rounded-lg object-cover'
-                                    />
+                                />
+                                <button onClick={() => removePhoto(link)} className="absolute bottom-2 right-2 text-white bg-black opacity-50 p-2 rounded-2xl">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                    </svg>
+
+                                </button>
                             </div>
                         ))
                     }
