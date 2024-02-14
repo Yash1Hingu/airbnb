32119f2410e8b269cac4152c0331d8e124208181
Yash1Hingu <yash23hingu@gmail.com>
2024-02-14 13:50:10 +0530

    Added loader in small

--- client/src/components/PageUI/PlaceLoader.jsx
+++ client/src/components/PageUI/PlaceLoader.jsx
@@ -3,10 +3,12 @@ export default function () {
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-12 mt-8">
             {
                 [1, 2, 3, 4, 5, 6].map(number => (
-                    <div className="loading-container aspect-square"
+                    <div className="loading-container aspect-square relative"
                         key={number}
                     >
                         <div className="loading-animation aspect-square w-full rounded-2xl"></div>
+                        <div className="loading-animation absolute w-[200px] h-10 bottom-24 left-7 rounded-2xl"></div>
+                        <div className="loading-animation absolute w-[200px] h-10 bottom-10 left-7 rounded-2xl"></div>
                     </div>
                 ))
             }
