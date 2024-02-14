fb2cacad008c3cba0c4ff977fe6365d6263eee20
Yash1Hingu <yash23hingu@gmail.com>
2024-02-15 01:13:37 +0530

    Added Bookings Page

--- client/src/components/pages/BookingsPage.jsx
+++ client/src/components/pages/BookingsPage.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { dateShow } from "../../util/dateShow";
+import { differenceInCalendarDays, format } from "date-fns";
 
 export default function BookingsPage() {
     const [bookings, setBookings] = useState([]);
@@ -10,9 +12,41 @@ export default function BookingsPage() {
         })
     }, [])
     return (
-        <div>
-            {bookings.map(place => (
-                <div>{place.checkIn}</div>
+        <div className="mt-8">
+            {bookings.map((data) => (
+                <div className="flex h-[200px] bg-gray-300 gap-2 mb-4 overflow-hidden rounded-2xl cursor-pointer">
+                    <div className="p-2 flex overflow-hidden">
+                        <img className="aspect-square object-cover rounded-xl" src={data.place?.photos[0]} alt="" />
+                    </div>
+                    <div className="flex flex-col justify-center">
+                        <h2 className="font-bold mt-2 text-xl">{data.place?.title}</h2>
+                        <h3 className="flex gap-1 text-gray-900 mt-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                            </svg>
+                            <span>
+                                {data.checkIn && `${dateShow(format(data.checkIn, 'yyyy-MM-dd'))}`}
+                            </span> -
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                            </svg>
+                            <span>
+                                {data.checkOut && `${dateShow(format(data.checkOut, 'yyyy-MM-dd'))}`}
+                            </span>
+                        </h3>
+                        <h3 className="flex gap-2 mt-3 text-black font-bold">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                            </svg>
+                            {(data.checkIn && data.checkOut) && differenceInCalendarDays(new Date(data.checkOut), new Date(data.checkIn))} night</h3>
+                        <h3 className="flex gap-2 font-bold mt-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM9 7.5A.75.75 0 0 0 9 9h1.5c.98 0 1.813.626 2.122 1.5H9A.75.75 0 0 0 9 12h3.622a2.251 2.251 0 0 1-2.122 1.5H9a.75.75 0 0 0-.53 1.28l3 3a.75.75 0 1 0 1.06-1.06L10.8 14.988A3.752 3.752 0 0 0 14.175 12H15a.75.75 0 0 0 0-1.5h-.825A3.733 3.733 0 0 0 13.5 9H15a.75.75 0 0 0 0-1.5H9Z" clipRule="evenodd" />
+                            </svg>
+                            {data?.price}
+                        </h3>
+                    </div>
+                </div>
             ))}
         </div>
     )
